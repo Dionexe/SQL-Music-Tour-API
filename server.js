be248a2df088c0c61a158c588a3fb14000da9a72
Dion Wells <dionwells@Dions-MacBook-Pro.local>
2024-04-04 11:28:19 -0700

    Added Migration

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 // DEPENDENCIES
+const { Sequelize } = require('sequelize')
 const express = require('express')
 const app = express()
-
+  
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+// SEQUELIZE CONNECTION
+// const sequelize = new Sequelize({
+//     storage: process.env.PG_URI,
+//     dialect: 'postgres',
+//     username: 'postgres',
+//     password: 'my_password'
+//   })
 
 // ROOT
 app.get('/', (req, res) => {
